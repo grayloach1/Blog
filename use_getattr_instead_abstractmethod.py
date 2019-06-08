847b6243a57d1a7bcbcc60812564f9d99ee33e4e
grayloach1 <35656857+grayloach1@users.noreply.github.com>
2019-06-08 08:44:54 +0800

    Update use_getattr_instead_abstractmethod.py

--- use_getattr_instead_abstractmethod.py
+++ use_getattr_instead_abstractmethod.py
@@ -5,7 +5,7 @@
 为此抽象基类必须引入两种高级的工具：函数装饰器和元类声明。既然抽象基类
 的目的是在直接实例化基类或者实例化未实现期望方法的子类时引发错误，从而
 实现检查的目的（示例见Super1类），那么是不是可以简单的在专门初始化实例
-的__init__()函数中，使用ggetattr(self, 'method')来检查实例是否定义了
+的__init__()函数中，使用getattr(self, 'method')来检查实例是否定义了
 method属性呢？示例见如下的Super2类。
 """
 from abc import ABCMeta, abstractmethod
